fe71ed3f81be0bf815dd83ed8fb385ccd7e05702
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-08-24 15:21:15 +0200

    Add triplet function, rename duration to Duration

--- include/durations.hpp
+++ include/durations.hpp
@@ -1,7 +1,7 @@
 #ifndef DURATIONS_HPP
 #define DURATIONS_HPP
 
-enum duration {
+enum Duration {
     D_FULL = 1,
     D_HALF = 2,
     D_QUARTER = 4,
@@ -9,8 +9,12 @@ enum duration {
     D_SIXTEENTH = 16
 };
 
-constexpr double dot(duration d) {
+constexpr double dot(Duration d) {
     return static_cast<double>(d) * 0.75;
+}
+
+constexpr double triplet(Duration d) {
+    return static_cast<double>(d) * 1.3;
 }
 
 #endif
